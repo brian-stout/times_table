25a1ad5015c520588756612eed6843f41b72c55e
Brian Stout <brianjonstout@gmail.com>
2016-10-21 10:37:23 -0400

    Add the functionality to dynamically determine the column width based on the product of the max number (The largest number.)

--- times_table.c
+++ times_table.c
@@ -18,20 +18,34 @@ int main(int argc, char *argv[])
         min_number = 1;
         max_number = strtol(argv[1], NULL, 10);    
     }
+    
+    //Determines the amount of space nessecary to accomade the largest product
+    int column_width = 1;
 
+    for(int i = (max_number * max_number); i > 0; i/=10){
+        column_width++;
+    }
+
+    //Determines the column_width of the first column
+    int first_column_width = 0;
+
+    for(int i = max_number; i > 0; i/=10){
+        first_column_width++;
+    }
 
     //Creates a header
-    printf("%4s", "*");
+    printf("%*s", first_column_width, "*");
+
     for(int i = min_number; i <= max_number; ++i){
-                printf("%4d", i);
+                printf("%*d", column_width, i);
     }
 
     printf("\n");
 
-    for(int i = min_number; i <= max_number; i++){
-            printf("%4d", i);
+    for(int i = min_number; i <= max_number; ++i){
+            printf("%*d", first_column_width, i);
             for(int n = min_number; n <= max_number; ++n){
-                printf("%4d", i * n);
+                printf("%*d", column_width, i * n);
             }
             printf("\n");
     }
