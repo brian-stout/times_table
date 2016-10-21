a74212efbda2f387128ec30f47d482f2674d31ef
Brian Stout <brianjonstout@gmail.com>
2016-10-21 08:40:22 -0400

    Have base times table program working.  Need to work on formatting.

--- times_table.c
+++ times_table.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main(void)
+{
+    int max_number = 10;
+
+
+    for(int i = 1; i <= max_number; ++i){
+            
+            for(int n = 1; n <= max_number; ++n){
+
+                printf("   %d", i * n);
+            }
+            printf("\n");
+        }
+
+
+
+
+}
