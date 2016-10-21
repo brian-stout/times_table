4ab1a73cf4c0c90169eaf0ab14be6d56aa6d925b
Brian Stout <brianjonstout@gmail.com>
2016-10-21 08:57:59 -0400

    Meet the base requirements.  Still need to add command line arguments.

--- times_table.c
+++ times_table.c
@@ -4,12 +4,21 @@ int main(void)
 {
     int max_number = 10;
 
+    for(int i = 0; i <= max_number; ++i){
+            if (i == 0){
+                printf("%4s", "*");
+            }
+            else{
+                printf("%4d", i);
+            }
+        }
+
+    printf("\n");
 
     for(int i = 1; i <= max_number; ++i){
-            
+            printf("%4d", i);
             for(int n = 1; n <= max_number; ++n){
-
-                printf("   %d", i * n);
+                printf("%4d", i * n);
             }
             printf("\n");
         }
